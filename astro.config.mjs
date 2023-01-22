7f507d68a5cad8f5498a9aebecb63a44a1500392
yama-sitter <dadayamism@gmail.com>
2023-01-23 00:27:57 +0900

    develop baseline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'http://yama-sitter.me',
+  integrations: [sitemap(), robotsTxt()],
+  // output: 'server',
+  // adapter: cloudflare({ mode: 'directory' }),
+});
---
+++ src/content/test.md
@@ -0,0 +1,10 @@
+---
+title: てすと
+date: 2023-01-22
+author: yama_sitter
+desc: てすと
+img: https://placehold.jp/1280x630.png
+imgWidth: 800
+---
+
+てすと
