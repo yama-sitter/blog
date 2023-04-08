210f25241e29ecab0977f9339fcd204f86eddd77
yama_sitter <dadayamism@gmail.com>
2023-04-08 22:59:16 +0900

    adjust dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import cloudflare from '@astrojs/cloudflare';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import rome from "astro-rome";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://yama-sitter.me',
-  integrations: [sitemap(), robotsTxt(), react()],
-  output: 'server',
+  site: "http://yama-sitter.me",
+  integrations: [sitemap(), robotsTxt(), react(), rome(), tailwind()],
+  output: "server",
   adapter: cloudflare({
-    mode: 'directory'
+    mode: "directory"
   })
 });
