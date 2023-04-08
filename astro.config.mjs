e3c70c6b70ba92d9dcdbc62993aca82e5c4a5cd4
yama_sitter <dadayamism@gmail.com>
2023-04-08 22:34:09 +0900

    introduce react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import cloudflare from '@astrojs/cloudflare';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://yama-sitter.me',
-  integrations: [sitemap(), robotsTxt()],
+  integrations: [sitemap(), robotsTxt(), react()],
   output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
+  adapter: cloudflare({
+    mode: 'directory'
+  })
 });
