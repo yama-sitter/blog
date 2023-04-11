20d42dc82025f90014e49231682f8acca2d9f47f
yama_sitter <dadayamism@gmail.com>
2023-04-11 22:08:23 +0900

    replace Preview component to React

---
+++ src/components/Preview.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import type { Article } from "@lib/api";
+import { stripHtml } from "@lib/strip-html";
+import { DateBadge } from "@tmp-components/DateBadge";
+
+export type Props = {
+  article: Article;
+};
+
+export const Preview: React.FC<Props> = ({ article }) => {
+  const foreword = useMemo(() => stripHtml(article.body), [article]);
+
+  return (
+    <div className="py-6 flex flex-row-reverse justify-between md:gap-10 items-start">
+      {article.image && (
+        <a href={`/${article.id}`} className="shrink-0">
+          <img
+            src={article.image.url}
+            alt={article.title}
+            width="160"
+            height="160"
+            className="w-20 h-20 md:w-40 md:h-40 rounded bg-base-200 object-cover"
+          />
+        </a>
+      )}
+      <div className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold">
+          <a className="hover:underline" href={`/${article.id}`}>
+            {article.title}
+          </a>
+        </h2>
+        <DateBadge date={article.publishedAt} />
+        <p className="text-sm text-base-content/70 truncated">{foreword}</p>
+      </div>
+    </div>
+  );
+};
