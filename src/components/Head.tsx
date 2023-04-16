d35609311160852e4f9f74cf653d3a6d2b2dfec9
yama-sitter <dadayamism@gmail.com>
2023-04-16 22:52:10 +0900

    fix title and description

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,11 +1,15 @@
 export type Props = {
+  title: string;
+  description: string;
   children?: React.ReactNode;
 };
 
-export const Head: React.FC<Props> = ({ children }) => (
+export const Head: React.FC<Props> = ({ title, description, children }) => (
   <head>
+    <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width" />
+    <meta name="description" content={description} />
     {children}
   </head>
 );
--- src/components/OGP.tsx
+++ src/components/OGP.tsx
@@ -1,22 +1,22 @@
 export type Props = {
   title: string;
   description: string;
-  image: string;
+  image?: string;
   url: string;
 };
 
 export const OGP: React.FC<Props> = ({ title, description, image, url }) => (
   <>
     <meta name="twitter:card" content="summary_large_image" />
-    {title && <meta name="twitter:title" content={title} />}
-    {description && <meta name="twitter:description" content={description} />}
-    <meta name="twitter:image" content={image} />
-    {title && <meta name="twitter:image:alt" content={title} />}
-    {title && <meta property="og:title" content={title} />}
-    {description && <meta property="og:description" content={description} />}
-    <meta property="og:image" content={image} />
-    {title && <meta property="og:image:alt" content={title} />}
-    {title && <meta property="og:site_name" content={title} />}
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    {image && <meta name="twitter:image" content={image} />}
+    <meta name="twitter:image:alt" content={title} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    {image && <meta property="og:image" content={image} />}
+    <meta property="og:image:alt" content={title} />
+    <meta property="og:site_name" content={title} />
     <meta property="og:type" content="object" />
     <meta property="og:url" content={url} />
   </>
