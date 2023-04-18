f27f010076496f9c3d4b2642b0e9e0549a5c337f
yama-sitter <dadayamism@gmail.com>
2023-04-18 22:13:03 +0900

    remove Footer component

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export type Props = {
-  children?: React.ReactNode;
-};
-
-export const Footer: React.FC<Props> = ({ children }) => (
-  <footer className="footer footer-center p-4">{children}</footer>
-);
