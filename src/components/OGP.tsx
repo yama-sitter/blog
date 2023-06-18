8f2ceedd88122efc3bb8b73ba748ea59e29311bc
yama-sitter <dadayamism@gmail.com>
2023-06-18 17:45:43 +0900

    avoid displaying unnecessary OGP tags when there is no image

--- src/components/OGP.tsx
+++ src/components/OGP.tsx
@@ -10,12 +10,20 @@ export const OGP: React.FC<Props> = ({ title, description, image, url }) => (
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    {image && <meta name="twitter:image" content={image} />}
-    <meta name="twitter:image:alt" content={title} />
+    {image && (
+      <>
+        <meta name="twitter:image" content={image} />
+        <meta name="twitter:image:alt" content={title} />
+      </>
+    )}
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    {image && <meta property="og:image" content={image} />}
-    <meta property="og:image:alt" content={title} />
+    {image && (
+      <>
+        <meta property="og:image" content={image} />
+        <meta property="og:image:alt" content={title} />
+      </>
+    )}
     <meta property="og:site_name" content={title} />
     <meta property="og:type" content="object" />
     <meta property="og:url" content={url} />
